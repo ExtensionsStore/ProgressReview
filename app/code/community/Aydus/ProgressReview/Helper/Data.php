e90701c5b7de43708dc2fd636fa0e9d9e6da3c40
David Tay <davidt@aydus.com>
2015-08-11 13:53:16 -0400

    ProgressReview extension

---
+++ app/code/community/Aydus/ProgressReview/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ * @category    Aydus
+ * @package     Aydus_ProgressReview
+ * @author      Aydus <davidt@aydus.com>
+ */
+class Aydus_ProgressReview_Helper_Data extends Mage_Core_Helper_Abstract 
+{
+    
+
+}
